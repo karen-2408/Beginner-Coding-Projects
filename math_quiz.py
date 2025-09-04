20bcd82d30da3d0c59998002694a7cd2355e7b4d
Karen Menghrajani <menghraj@ualberta.ca>
2025-09-04 17:33:35 -0600

    Create math_quiz.py

---
+++ math_quiz.py
@@ -0,0 +1,28 @@
+import math
+import time
+
+print("Welcome to my math quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+questions = 10
+
+for i in range(questions):
+  num1 = random.randint(0, 10000)
+  num2 = random.randint(0, 10000)
+  answer = input("What does " + num1+ "+ " + num2)
+  real_answer = num1+num2
+  if answer == real_answer:
+    print('Correct!')
+    score += 1
+  else:
+    print("Incorrect!")
+
+print("Quiz complete!")
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
